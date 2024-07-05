5a7ce0ded4d29208208303ed3b20d79722af2a54
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-07-05 07:14:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const http = require("http")
+
+const socketio = require("socket.io");
+
+
+const server = http.createServer(app);
+
+const io = socketio(server)
 
 app.get('/', function(res,req){
     res.setEncoding("hello")
