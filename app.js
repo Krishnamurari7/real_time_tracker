92cd110ba111576da87d2ef2e3722bf640cd10ae
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-07-05 07:29:35 +0530

    update

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
-const http = require("http")
+const http = require("http");
+const path = require('path');
 
 const socketio = require("socket.io");
 
 
 const server = http.createServer(app);
 
-const io = socketio(server)
+const io = socketio(server);
+
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(res,req){
     res.setEncoding("hello")
 });
 
-app.listen(3000);
+server.listen(3000);
